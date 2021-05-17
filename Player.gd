b508931f9ab9098c00dee25d0d3fd73375646784
bholifie <bholifie@redhat.com>
2021-05-17 10:44:39 -0500

    Camera is a child of player and follows. Adds basic obstacle with collision.

--- Player.gd
+++ Player.gd
@@ -4,46 +4,51 @@ signal hit
 
 export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
+var playerDisabled
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	playerDisabled = true
 	hide()
 
 
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+	if not playerDisabled:
+		var velocity = Vector2.ZERO # The player's movement vector.
+		if Input.is_action_pressed("move_right"):
+			velocity.x += 1
+		if Input.is_action_pressed("move_left"):
+			velocity.x -= 1
+		if Input.is_action_pressed("move_down"):
+			velocity.y += 1
+		if Input.is_action_pressed("move_up"):
+			velocity.y -= 1
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite.play()
-	else:
-		$AnimatedSprite.stop()
+		if velocity.length() > 0:
+			velocity = velocity.normalized() * speed
+			$AnimatedSprite.play()
+		else:
+			$AnimatedSprite.stop()
 
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+		position += velocity * delta
+		# clamp() will lock the player to the screen size. We don't want dis
+		# position.x = clamp(position.x, 0, screen_size.x)
+		# position.y = clamp(position.y, 0, screen_size.y)
 
-	if velocity.x != 0:
-		$AnimatedSprite.animation = "right"
-		$AnimatedSprite.flip_v = false
-		$AnimatedSprite.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite.animation = "up"
-		$AnimatedSprite.flip_v = velocity.y > 0
+		if velocity.x != 0:
+			$AnimatedSprite.animation = "right"
+			$AnimatedSprite.flip_v = false
+			$AnimatedSprite.flip_h = velocity.x < 0
+		elif velocity.y != 0:
+			$AnimatedSprite.animation = "up"
+			$AnimatedSprite.flip_v = velocity.y > 0
 
 
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
+	playerDisabled = false
 
 
 func _on_Player_body_entered(_body):
